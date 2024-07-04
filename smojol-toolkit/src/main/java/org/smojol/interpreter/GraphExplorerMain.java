21d0fdeba84104f887a279aecbf5ef998c6a0163
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-07-05 04:21:40 +0530

    Added Neo4JVisitor which imports Control Flow Graph into DB

---
+++ smojol-toolkit/src/main/java/org/smojol/interpreter/GraphExplorerMain.java
@@ -0,0 +1,57 @@
+package org.smojol.interpreter;
+
+import com.mojo.woof.GraphSDK;
+import com.mojo.woof.Neo4JDriverBuilder;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.smojol.analysis.LanguageDialect;
+import org.smojol.analysis.ParsePipeline;
+import org.smojol.analysis.visualisation.CobolTreeVisualiserImpl;
+import org.smojol.analysis.visualisation.PocOpsImpl;
+import org.smojol.ast.FlowchartBuilderImpl;
+import org.smojol.common.flowchart.FlowNode;
+import org.smojol.common.flowchart.FlowNodeService;
+import org.smojol.common.flowchart.FlowchartBuilder;
+import org.smojol.common.navigation.CobolEntityNavigator;
+import org.smojol.common.vm.interpreter.Breakpointer;
+import org.smojol.common.vm.strategy.UnresolvedReferenceDoNothingStrategy;
+import org.smojol.common.vm.structure.CobolDataStructure;
+import org.smojol.interpreter.interpreter.CobolBreakpointer;
+import org.smojol.interpreter.navigation.CobolEntityNavigatorBuilderImpl;
+
+import java.io.File;
+import java.io.IOException;
+
+public class GraphExplorerMain {
+    private final Logger logger = LoggerFactory.getLogger(GraphExplorerMain.class);
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        File[] copyBookPaths = new File[]{new File("/Users/asgupta/code/smojol/smojol-test-code")};
+        String dialectJarPath = "/Users/asgupta/code/smojol/che-che4z-lsp-for-cobol-integration/server/dialect-idms/target/dialect-idms.jar";
+        String cobolParseTreeOutputPath = "/Users/asgupta/code/smojol/out/test-cobol.json";
+
+        File source = new File("/Users/asgupta/code/smojol/smojol-test-code/test.cbl");
+
+        PocOpsImpl ops = new PocOpsImpl(new CobolTreeVisualiserImpl(),
+                FlowchartBuilderImpl::build, new CobolEntityNavigatorBuilderImpl(), new UnresolvedReferenceDoNothingStrategy());
+        ParsePipeline pipeline = new ParsePipeline(source,
+                copyBookPaths,
+                dialectJarPath,
+                cobolParseTreeOutputPath,
+                ops, LanguageDialect.COBOL);
+
+        CobolEntityNavigator navigator = pipeline.parse();
+        FlowchartBuilder flowcharter = pipeline.flowcharter();
+        CobolDataStructure dataStructures = pipeline.getDataStructures();
+
+        ParseTree procedure = navigator.procedureBodyRoot();
+
+        flowcharter.buildChartAST(procedure).buildControlFlow().buildOverlay();
+        FlowNode root = flowcharter.getRoot();
+        FlowNodeService nodeService = flowcharter.getChartNodeService();
+
+        GraphSDK sdk = new GraphSDK(new Neo4JDriverBuilder().fromEnv());
+        root.accept(new Neo4JVisitor(sdk), -1);
+    }
+}
---
+++ smojol-toolkit/src/main/java/org/smojol/interpreter/Neo4JVisitor.java
@@ -0,0 +1,60 @@
+package org.smojol.interpreter;
+
+import com.google.common.collect.ImmutableList;
+import com.mojo.woof.GraphSDK;
+import com.mojo.woof.WoofNode;
+import org.neo4j.driver.Record;
+import org.smojol.common.flowchart.*;
+
+import java.util.List;
+import java.util.Map;
+
+public class Neo4JVisitor implements FlowNodeVisitor {
+    private final GraphSDK sdk;
+
+    public Neo4JVisitor(GraphSDK sdk) {
+        this.sdk = sdk;
+    }
+
+    @Override
+    public void visit(FlowNode node, List<FlowNode> outgoingNodes, List<FlowNode> incomingNodes, VisitContext context, FlowNodeService nodeService) {
+        outgoingNodes.forEach(o -> {
+            Record sourceRecord = newOrExisting(node);
+            Record destinationRecord = newOrExisting(o);
+            sdk.connect(sourceRecord, destinationRecord, "FOLLOWED_BY");
+        });
+    }
+
+    @Override
+    public void visitParentChildLink(FlowNode parent, FlowNode internalTreeRoot, VisitContext ctx, FlowNodeService nodeService) {
+        Record parentRecord = newOrExisting(parent);
+        Record childRecord = newOrExisting(internalTreeRoot);
+
+        sdk.connect(parentRecord, childRecord, "STARTS_WITH");
+    }
+
+    private Record newOrExisting(FlowNode node) {
+        return sdk.newOrExisting(Map.of("flowID", node.id()), new WoofNode(Map.of("flowID", node.id(), "text", node.getExecutionContext().getText()), ImmutableList.of("CFG_NODE", node.type().toString())));
+    }
+
+    @Override
+    public void visitParentChildLink(FlowNode parent, FlowNode internalTreeRoot, VisitContext ctx, FlowNodeService nodeService, FlowNodeCondition hideStrategy) {
+        visitParentChildLink(parent, internalTreeRoot, ctx, nodeService);
+    }
+
+    @Override
+    public void visitControlTransfer(FlowNode from, FlowNode to, VisitContext visitContext) {
+        Record sourceRecord = newOrExisting(from);
+        Record destinationRecord = newOrExisting(to);
+        sdk.connect(sourceRecord, destinationRecord, "JUMPS_TO");
+    }
+    @Override
+    public FlowNodeVisitor newScope(FlowNode enclosingScope) {
+        return this;
+    }
+
+    @Override
+    public void group(FlowNode root) {
+
+    }
+}
