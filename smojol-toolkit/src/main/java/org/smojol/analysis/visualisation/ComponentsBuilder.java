627cbfd7ea2f70888af12b2915977cb4c867b6d9
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-07-11 16:38:51 +0530

    Using Lombok for a couple of things

--- smojol-toolkit/src/main/java/org/smojol/analysis/visualisation/ComponentsBuilder.java
+++ smojol-toolkit/src/main/java/org/smojol/analysis/visualisation/ComponentsBuilder.java
@@ -1,5 +1,6 @@
 package org.smojol.analysis.visualisation;
 
+import lombok.Getter;
 import org.smojol.common.navigation.CobolEntityNavigator;
 import org.smojol.common.navigation.EntityNavigatorBuilder;
 import org.smojol.common.vm.structure.Format1DataStructureBuilder;
@@ -11,19 +12,11 @@ import org.smojol.common.flowchart.FlowchartBuilderFactoryMethod;
 import org.smojol.common.vm.structure.DataStructureBuilder;
 
 public class ComponentsBuilder {
-    private final CobolTreeVisualiser visualiser;
-    private final FlowchartBuilderFactoryMethod flowchartBuilderFactory;
+    @Getter private final CobolTreeVisualiser visualiser;
+    @Getter private final FlowchartBuilderFactoryMethod flowchartBuilderFactory;
     private final CobolEntityNavigatorBuilderImpl cobolEntityNavigatorBuilder;
     private final UnresolvedReferenceStrategy unresolvedReferenceStrategy;
     private final Format1DataStructureBuilder format1DataStructureBuilder;
-
-    public CobolTreeVisualiser getVisualiser() {
-        return visualiser;
-    }
-
-    public FlowchartBuilderFactoryMethod getFlowchartBuilderFactory() {
-        return flowchartBuilderFactory;
-    }
 
     public ComponentsBuilder(CobolTreeVisualiser visualiser, FlowchartBuilderFactoryMethod flowchartBuilderFactory, CobolEntityNavigatorBuilderImpl cobolEntityNavigatorBuilder, UnresolvedReferenceStrategy unresolvedReferenceStrategy, Format1DataStructureBuilder format1DataStructureBuilder) {
         this.visualiser = visualiser;
