8bb8c20a437523c9b8031e90ed324b004a90e36c
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-07-13 06:44:24 +0530

    Refactoring out redefinition pair computation prior to using it in GraphML export

--- smojol-toolkit/src/main/java/org/smojol/analysis/graph/Neo4JRedefinitionVisitor.java
+++ smojol-toolkit/src/main/java/org/smojol/analysis/graph/Neo4JRedefinitionVisitor.java
@@ -1,6 +1,7 @@
 package org.smojol.analysis.graph;
 
 import com.mojo.woof.GraphSDK;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.eclipse.lsp.cobol.core.CobolParser;
 import org.neo4j.driver.Record;
 import org.smojol.common.flowchart.DataStructureVisitor;
@@ -11,6 +12,7 @@ import java.util.Map;
 
 import static com.mojo.woof.NodeProperties.NAME;
 import static com.mojo.woof.NodeRelations.REDEFINES;
+import static org.smojol.analysis.graph.DataRedefinitionComputer.redefinitionPair;
 
 public class Neo4JRedefinitionVisitor implements DataStructureVisitor {
     private final GraphSDK sdk;
@@ -23,15 +25,18 @@ public class Neo4JRedefinitionVisitor implements DataStructureVisitor {
 
     @Override
     public CobolDataStructure visit(CobolDataStructure data, CobolDataStructure parent, CobolDataStructure root) {
-        if (!(data instanceof Format1DataStructure)) return data;
-        CobolParser.DataDescriptionEntryFormat1Context dataDescription = ((Format1DataStructure) data).getDataDescription();
-        if (dataDescription == null || dataDescription.dataRedefinesClause().isEmpty()) return data;
-        String redefinedRecordName = dataDescription.dataRedefinesClause().getFirst().dataName().getText();
-        CobolDataStructure redefinedStruct = root.reference(redefinedRecordName);
-        Record redefinition = sdk.findNodes(nodeQualifier.dataNodeSearchSpec(data)).getFirst();
-        Record redefinedRecord = sdk.findNodes(nodeQualifier.dataNodeSearchSpec(redefinedStruct)).getFirst();
+//        if (!(data instanceof Format1DataStructure)) return data;
+//        CobolParser.DataDescriptionEntryFormat1Context dataDescription = ((Format1DataStructure) data).getDataDescription();
+//        if (dataDescription == null || dataDescription.dataRedefinesClause().isEmpty()) return data;
+//        String redefinedRecordName = dataDescription.dataRedefinesClause().getFirst().dataName().getText();
+//        CobolDataStructure redefinedStruct = root.reference(redefinedRecordName);
+        Map.Entry<CobolDataStructure, CobolDataStructure> redefinitionPair = redefinitionPair(data, root);
+        if (ImmutablePair.nullPair().equals(redefinitionPair)) return data;
+        Record redefinition = sdk.findNodes(nodeQualifier.dataNodeSearchSpec(redefinitionPair.getKey())).getFirst();
+        Record redefinedRecord = sdk.findNodes(nodeQualifier.dataNodeSearchSpec(redefinitionPair.getValue())).getFirst();
         sdk.connect(redefinition, redefinedRecord, REDEFINES);
         return data;
     }
+
 }
 
