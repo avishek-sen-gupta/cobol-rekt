90e89ec95735d81cdfa67f1adc02821b8fa93d74
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-08-12 16:35:50 +0530

    Making dependency tracing more robust, terminating recursion in ill-posed programs

--- smojol-toolkit/src/main/java/org/smojol/analysis/pipeline/AnalyseProgramDependenciesTask.java
+++ smojol-toolkit/src/main/java/org/smojol/analysis/pipeline/AnalyseProgramDependenciesTask.java
@@ -2,10 +2,12 @@ package org.smojol.analysis.pipeline;
 
 import com.google.common.collect.ImmutableList;
 import hu.webarticum.treeprinter.printer.listing.ListingTreePrinter;
+import org.apache.commons.io.FileUtils;
 import org.smojol.analysis.LanguageDialect;
 import org.smojol.analysis.visualisation.CobolProgram;
 import org.smojol.ast.ProgramDependencies;
 import org.smojol.ast.StaticCallTarget;
+import org.smojol.common.flowchart.ConsoleColors;
 import org.smojol.common.flowchart.FlowchartOutputFormat;
 import org.smojol.common.id.UUIDProvider;
 import org.smojol.interpreter.FullProgram;
@@ -13,6 +15,7 @@ import org.smojol.interpreter.structure.OccursIgnoringFormat1DataStructureBuilde
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -32,19 +35,33 @@ public class AnalyseProgramDependenciesTask {
     }
 
     private void recurse(CobolProgram program, List<File> copyBookPaths) throws IOException {
-        Map<String, List<AnalysisTaskResult>> results = new CodeTaskRunner(sourceDir,
-                reportRootDir, copyBookPaths, dialectJarPath,
-                LanguageDialect.IDMS, new FullProgram(FlowchartOutputFormat.SVG), new UUIDProvider(), new OccursIgnoringFormat1DataStructureBuilder())
-                .generateForPrograms(ImmutableList.of(BUILD_PROGRAM_DEPENDENCIES), ImmutableList.of(program.getName()));
-        AnalysisTaskResult first = results.get(program.getName()).getFirst();
-        ProgramDependencies dependencies = switch (first) {
-            case AnalysisTaskResultOK o -> (ProgramDependencies) o.getDetail();
-            case AnalysisTaskResultError e -> throw new RuntimeException(e.getException());
-        };
-        program.addAll(dependencies.getDependencies().stream().map(CobolProgram::new).toList());
+        System.out.println("Searching for program: " + program.getName() + "...");
+        Collection<File> files = FileUtils.listFiles(new File(sourceDir), null, true);
+        List<File> matchingFiles = files.stream().filter(f -> program.getName().equals(f.getName())).toList();
+        if (matchingFiles.isEmpty()) {
+            System.out.println(ConsoleColors.red("No files matching " + program.getName() + " in " + sourceDir + ". Terminating recursion..."));
+            return;
+        }
+        File foundFile = matchingFiles.getFirst();
+        String srcDir = foundFile.getParent();
+        System.out.println("Found " + foundFile.getName() + " in " + srcDir);
+        try {
+            Map<String, List<AnalysisTaskResult>> results = new CodeTaskRunner(srcDir,
+                    reportRootDir, copyBookPaths, dialectJarPath,
+                    LanguageDialect.IDMS, new FullProgram(FlowchartOutputFormat.SVG), new UUIDProvider(), new OccursIgnoringFormat1DataStructureBuilder())
+                    .generateForPrograms(ImmutableList.of(BUILD_PROGRAM_DEPENDENCIES), ImmutableList.of(program.getName()));
+            AnalysisTaskResult first = results.get(program.getName()).getFirst();
+            ProgramDependencies dependencies = switch (first) {
+                case AnalysisTaskResultOK o -> (ProgramDependencies) o.getDetail();
+                case AnalysisTaskResultError e -> throw new RuntimeException(e.getException());
+            };
+            program.addAll(dependencies.getDependencies().stream().map(CobolProgram::new).toList());
 
-        for (CobolProgram childDependency : program.staticDependencies())
-            recurse(childDependency, copyBookPaths);
+            for (CobolProgram childDependency : program.staticDependencies())
+                recurse(childDependency, copyBookPaths);
+        } catch (IOException | RuntimeException e) {
+            System.out.println(ConsoleColors.red("Error, terminating recursion down this path. Error: " + e.getMessage()));
+        }
     }
 
     public AnalysisTaskResult run(String programName) throws IOException {
