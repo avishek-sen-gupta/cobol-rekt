5789fab4ddaa43547443e4487c76e0e3638d7ef8
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-08-19 12:37:21 +0530

    Updating frequent pattern mining to be parameterised for input files

--- smojol_python/src/analysis/frequent_pattern_miner.py
+++ smojol_python/src/analysis/frequent_pattern_miner.py
@@ -1,27 +1,43 @@
-import os
+import argparse
+import json
 
-from src.gspan_mining.gspan import gSpan
 from dotenv import load_dotenv
+
+from src.common.flow_node import FlowNode
+from src.gspan_mining.gspan import gSpan
+from src.ingestion.unified_model_to_networkx import extract_cfg
+from src.llm.common.parameter_constants import ParameterConstants
 
 load_dotenv("env/.env", override=True)
 
 if __name__ == "__main__":
-    neo4j_uri = os.environ.get("NEO4J_URI")
-    neo4j_username = os.environ.get("NEO4J_USERNAME")
-    neo4j_password = os.environ.get("NEO4J_PASSWORD")
-    neo4j_database = os.environ.get("NEO4J_DATABASE", "neo4j")
-    cg = gSpan(
-        database_file_name="/Users/asgupta/code/smojol/smojol_python/simple.txt",
-        min_support=1,
-        min_num_vertices=8,
-        max_num_vertices=15,
-        is_undirected=False,
-        verbose=True,
-        visualize=True
-    )
+    parser = argparse.ArgumentParser(prog="frequent_pattern_mining")
+    parser.add_argument(ParameterConstants.UNIFIED_MODEL_PATH)
+    args = parser.parse_args()
+    input_path = getattr(args, ParameterConstants.UNIFIED_MODEL_PATH)
+    with open(input_path, 'r') as file:
+        unified = json.load(file)
+        cfg_x, _, _ = extract_cfg(unified)
+        node_integer_mapping: dict[int, FlowNode] = {}
+        all_nodes = cfg_x.nodes
+        all_edges = cfg_x.edges
 
-    cg.run()
-    cg.time_stats()
-    # return cg
-    # paths = match_pattern((neo4j_uri, (neo4j_username, neo4j_password), neo4j_database))
-    print("DONE")
+        flow_ids = list(map(lambda ix, node: (ix, node.node_category, node), enumerate(all_nodes)))
+        for flow_id, node in flow_ids:
+            node_integer_mapping[flow_id] = node
+
+        cg = gSpan(
+            database_file_name="/Users/asgupta/code/smojol/smojol_python/test_data/simple.txt",
+            min_support=1,
+            min_num_vertices=8,
+            max_num_vertices=15,
+            is_undirected=False,
+            verbose=True,
+            visualize=True
+        )
+
+        cg.run()
+        cg.time_stats()
+        # return cg
+        # paths = match_pattern((neo4j_uri, (neo4j_username, neo4j_password), neo4j_database))
+        print("DONE")
