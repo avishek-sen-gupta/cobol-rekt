5f40cefef9a28060010677b9aa9cd1940ddd3e3e
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-08-27 22:24:36 +0530

    Flexible JSON traversing, functional form

--- smojol_python/src/analysis/variable_values.py
+++ smojol_python/src/analysis/variable_values.py
@@ -1,5 +1,7 @@
 import argparse
 import json
+from functools import reduce
+
 from jsonpath_ng.ext import parse
 
 from dotenv import load_dotenv
@@ -20,60 +22,52 @@ def __getitem__(self, item):
     return self.dict[item]
 
 
-def direct(node_type):
+def lol(final, c):
+    if not isinstance(final, NullNode):
+        return final
+    if not isinstance(c, NullNode):
+        return c
+    return NullNode()
+
+
+def direct(criterion):
     def apply(node):
-        if isinstance(node, NullNode):
-            return NullNode()
-        children = node["children"]
-        if len(children) == 0:
-            return NullNode()
-        found_children = list(filter(lambda n: n["nodeType"] == node_type, children))
-        if len(found_children) == 0:
-            return NullNode()
-        return found_children[0]
+        found_children = list(filter(lambda n: criterion(n), node["children"]))
+        return reduce(lol, found_children, NullNode())
 
     return apply
 
 
-def recursive(node_type):
+def recursive(criterion):
     def apply(node):
         print(f"Checking node of type: {node['nodeType']}")
-        if isinstance(node, NullNode):
-            return NullNode()
-        if node["nodeType"] == node_type:
+        if criterion(node):
             return node
-        if len(node["children"]) == 0:
-            return NullNode()
-        for child in node["children"]:
-            found_descendant = apply(child)
-            if isinstance(found_descendant, NullNode):
-                continue
-            return found_descendant
-        return NullNode()
+        return reduce(lol, list(map(apply, node["children"])), NullNode())
 
     return apply
 
 
-def recurse_operand(node_type, tree):
+def recurse_operand(criterion, tree):
     matching = []
-    if tree["nodeType"] == node_type:
+    if criterion(tree):
         return [tree]
+    # map(lambda n: recurse_operand(criterion, node), tree["children"])
     for node in tree["children"]:
-        matching += recurse_operand(node_type, node)
+        matching += recurse_operand(criterion, node)
     return matching
 
 
 def flter(tree, node_type, conditions):
     matching_nodes = recurse_operand(node_type, tree)
-    final_nodes = []
-    for node in matching_nodes:
-        current_level = node
-        for condition in conditions:
-            current_level = condition(current_level)
-        if isinstance(current_level, NullNode):
-            continue
-        final_nodes.append(node)
-    return final_nodes
+    return list(filter(lambda node: not isinstance(reduce(lambda prev, search: search(prev), conditions, node), NullNode), matching_nodes))
+
+
+def node_type(ntype):
+    def apply(node):
+        return node["nodeType"] == ntype
+
+    return apply
 
 
 if __name__ == "__main__":
@@ -86,8 +80,9 @@ if __name__ == "__main__":
 
     with open(input_path, 'r') as file:
         ast = json.load(file)
-        final_nodes = flter(ast, "AddStatementContext",
-                            [recursive("AddFromContext"), recursive("GeneralIdentifierContext")])
+        final_nodes = flter(ast, node_type("AddStatementContext"),
+                            [recursive(node_type("AddFromContext")),
+                                       recursive(node_type("GeneralIdentifierContext"))])
         # jp = parse("$..*[?(@.nodeType == 'VariableUsageNameContext' & @.children[0].nodeType == 'CobolWordContext')]")
         # jp = parse(
         #     "$..*[?(@.nodeType == 'VariableUsageNameContext' & @.`parent`.nodeType == 'MoveToStatementContext')]")
