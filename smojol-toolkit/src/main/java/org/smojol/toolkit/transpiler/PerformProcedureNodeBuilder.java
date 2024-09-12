50fae085b168191f26dcc26f5001bdad892a932e
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-09-12 23:12:13 +0530

    Fixing a stray compile error

--- smojol-toolkit/src/main/java/org/smojol/toolkit/transpiler/PerformProcedureNodeBuilder.java
+++ smojol-toolkit/src/main/java/org/smojol/toolkit/transpiler/PerformProcedureNodeBuilder.java
@@ -10,9 +10,6 @@ import org.smojol.toolkit.ast.PerformProcedureFlowNode;
 
 public class PerformProcedureNodeBuilder {
     public static TranspilerNode build(PerformProcedureFlowNode n, CobolDataStructure dataStructures) {
-        if (!n.dependsUponFactor()) return new JumpNode(new LocationNode(n.callTargets().getFirst().name()));
-        TranspilerExpressionBuilder nodeBuilder = new TranspilerExpressionBuilder(dataStructures);
-        ValueOfNode factorValue = new ValueOfNode(nodeBuilder.build(n.getDependingFactor()));
-        return ladder(n, factorValue, 0);
+        return null;
     }
 }
