6bf428ef47f8537618f81092b78934ba10dae1f7
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-09-24 02:36:28 +0530

    Implementing Immediate Dominator algorithm from Allen and Cocke (1972)

--- smojol-core/src/main/java/org/smojol/common/graph/DepthFirstTraversalLabelTask.java
+++ smojol-core/src/main/java/org/smojol/common/graph/DepthFirstTraversalLabelTask.java
@@ -1,17 +1,21 @@
 package org.smojol.common.graph;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.smojol.common.list.ConsCar.tail;
 
 public class DepthFirstTraversalLabelTask {
     public static final String DFS_NUM = "DFS_NUM";
     private final List<GraphNodeLike> depthFirstSpanningTreeOrder = new ArrayList<>();
     private final GraphNodeLike root;
     private final Graph<GraphNodeLike, DefaultEdge> graph;
-    private int currentDfsNumber = 0;
+    private int currentDfsNumber;
 
     public DepthFirstTraversalLabelTask(GraphNodeLike root, Graph<GraphNodeLike, DefaultEdge> graph) {
         this(root, graph, 0);
@@ -49,5 +53,60 @@ public class DepthFirstTraversalLabelTask {
 
     public List<GraphNodeLike> postOrder() {
         return depthFirstSpanningTreeOrder.reversed();
+    }
+
+    public Map<GraphNodeLike, Set<GraphNodeLike>> allDominators() {
+        run(root);
+        List<GraphNodeLike> order = preOrder();
+        Set<GraphNodeLike> allNodes = new HashSet<>(order);
+        Map<GraphNodeLike, Set<GraphNodeLike>> dominators = new HashMap<>();
+        dominators.put(order.getFirst(), Set.of(order.getFirst()));
+        List<GraphNodeLike> tail = tail(order);
+        tail.forEach(node -> dominators.put(node, new HashSet<>(order)));
+
+        do {
+
+        } while (tail.stream().map(n -> {
+            Set<GraphNodeLike> predecessors = graph.incomingEdgesOf(n).stream().map(graph::getEdgeSource).collect(Collectors.toUnmodifiableSet());
+            Set<GraphNodeLike> dominatorSet = new HashSet<>();
+            dominatorSet.add(n);
+            List<Set<GraphNodeLike>> predDominators = predecessors.stream().map(dominators::get).toList();
+            Set<GraphNodeLike> finalIntersection = predDominators.stream().reduce(allNodes, (a, b) -> {
+                HashSet<GraphNodeLike> intersection = new HashSet<>(a);
+                intersection.retainAll(b);
+                return intersection;
+            });
+            dominatorSet.addAll(finalIntersection);
+            if (dominatorSet.equals(dominators.get(n))) {
+                dominators.put(n, dominatorSet);
+                return false;
+            }
+            dominators.put(n, dominatorSet);
+            return true;
+        }).reduce(false, (a, b) -> a || b));
+
+        return dominators;
+    }
+
+    public List<ImmutablePair<GraphNodeLike, GraphNodeLike>> immediateDominators() {
+        Map<GraphNodeLike, Set<GraphNodeLike>> allDominators = allDominators();
+        List<Pair<GraphNodeLike, GraphNodeLike>> dominances = allDominators.entrySet().stream().flatMap(e -> e.getValue().stream().map(dom -> (Pair<GraphNodeLike, GraphNodeLike>) ImmutablePair.of(dom, e.getKey()))).toList();
+        return allDominators.entrySet().stream().map(e -> ImmutablePair.of(e.getKey(), uniqueImmediateDominator(e.getKey(), e.getValue(), dominances))).toList();
+    }
+
+    private GraphNodeLike uniqueImmediateDominator(GraphNodeLike node, Set<GraphNodeLike> potentialImmediateDominators, List<Pair<GraphNodeLike, GraphNodeLike>> dominances) {
+        if (node == root) return node;
+        HashSet<GraphNodeLike> xxxx = new HashSet<>(potentialImmediateDominators);
+        List<GraphNodeLike> removals = new ArrayList<>();
+        xxxx.remove(node);
+        for (GraphNodeLike d1 : xxxx) {
+            for (GraphNodeLike d2 : xxxx) {
+                if (d1 == d2) continue;
+                if (dominances.contains(ImmutablePair.of(d1, d2))) removals.add(d1);
+            }
+        }
+
+        removals.forEach(xxxx::remove);
+        return xxxx.stream().findFirst().get();
     }
 }
--- smojol-core/src/test/java/org/poc/common/DepthFirstTraversalLabelTaskTest.java
+++ smojol-core/src/test/java/org/poc/common/DepthFirstTraversalLabelTaskTest.java
@@ -1,6 +1,8 @@
 package org.poc.common;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
@@ -9,7 +11,12 @@ import org.smojol.common.graph.CodeGraphNode;
 import org.smojol.common.graph.DepthFirstTraversalLabelTask;
 import org.smojol.common.graph.GraphNodeLike;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.smojol.common.graph.DepthFirstTraversalLabelTask.DFS_NUM;
 
 public class DepthFirstTraversalLabelTaskTest {
@@ -34,15 +41,21 @@ public class DepthFirstTraversalLabelTaskTest {
 
     @Test
     public void canLabelDFSOrderForSimpleGraphWithBranches() {
+    /*
+    0 -> 1 -> 2 -> 3 -> 4 -> 5
+         |    |    ↑         ↑
+         |____|____|         |
+              └---> 6 --> 7 -┘
+     */
         Graph<GraphNodeLike, DefaultEdge> graph = new DefaultDirectedGraph<>(DefaultEdge.class);
-        CodeGraphNode v0 = new CodeGraphNode("A");
-        CodeGraphNode v1 = new CodeGraphNode("B");
-        CodeGraphNode v2 = new CodeGraphNode("C");
-        CodeGraphNode v3 = new CodeGraphNode("D");
-        CodeGraphNode v4 = new CodeGraphNode("E");
-        CodeGraphNode v5 = new CodeGraphNode("F");
-        CodeGraphNode v6 = new CodeGraphNode("G");
-        CodeGraphNode v7 = new CodeGraphNode("H");
+        CodeGraphNode v0 = new CodeGraphNode("0");
+        CodeGraphNode v1 = new CodeGraphNode("1");
+        CodeGraphNode v2 = new CodeGraphNode("2");
+        CodeGraphNode v3 = new CodeGraphNode("3");
+        CodeGraphNode v4 = new CodeGraphNode("4");
+        CodeGraphNode v5 = new CodeGraphNode("5");
+        CodeGraphNode v6 = new CodeGraphNode("6");
+        CodeGraphNode v7 = new CodeGraphNode("7");
 
         graph.addVertex(v0);
         graph.addVertex(v1);
@@ -53,7 +66,7 @@ public class DepthFirstTraversalLabelTaskTest {
         graph.addVertex(v6);
         graph.addVertex(v7);
         graph.addEdge(v0, v1);
-        graph.addEdge(v0, v2);
+        graph.addEdge(v1, v2);
         graph.addEdge(v2, v3);
         graph.addEdge(v3, v4);
         graph.addEdge(v4, v5);
@@ -70,10 +83,10 @@ public class DepthFirstTraversalLabelTaskTest {
         task.run();
         assertEquals(0, v0.getProperty(DFS_NUM, Integer.class));
         assertEquals(1, v1.getProperty(DFS_NUM, Integer.class));
-        assertEquals(5, v2.getProperty(DFS_NUM, Integer.class));
-        assertEquals(2, v3.getProperty(DFS_NUM, Integer.class));
-        assertEquals(3, v4.getProperty(DFS_NUM, Integer.class));
-        assertEquals(4, v5.getProperty(DFS_NUM, Integer.class));
+        assertEquals(2, v2.getProperty(DFS_NUM, Integer.class));
+        assertEquals(3, v3.getProperty(DFS_NUM, Integer.class));
+        assertEquals(4, v4.getProperty(DFS_NUM, Integer.class));
+        assertEquals(5, v5.getProperty(DFS_NUM, Integer.class));
         assertEquals(6, v6.getProperty(DFS_NUM, Integer.class));
         assertEquals(7, v7.getProperty(DFS_NUM, Integer.class));
         assertEquals(8, task.max());
@@ -137,5 +150,111 @@ public class DepthFirstTraversalLabelTaskTest {
         assertEquals(9, task.max());
         assertEquals(ImmutableList.of(v1, v2, v3, v4, v5, v6, v7, v8), task.preOrder());
         assertEquals(ImmutableList.of(v8, v7, v6, v5, v4, v3, v2, v1), task.postOrder());
+    }
+
+    @Test
+    public void canFindDominatorSets() {
+    /*
+    0 -> 1 -> 2 -> 3 -> 4 -> 5
+         |    |    ↑         ↑
+         |____|____|         |
+              └---> 6 --> 7 -┘
+     */
+        Graph<GraphNodeLike, DefaultEdge> graph = new DefaultDirectedGraph<>(DefaultEdge.class);
+        CodeGraphNode v0 = new CodeGraphNode("0");
+        CodeGraphNode v1 = new CodeGraphNode("1");
+        CodeGraphNode v2 = new CodeGraphNode("2");
+        CodeGraphNode v3 = new CodeGraphNode("3");
+        CodeGraphNode v4 = new CodeGraphNode("4");
+        CodeGraphNode v5 = new CodeGraphNode("5");
+        CodeGraphNode v6 = new CodeGraphNode("6");
+        CodeGraphNode v7 = new CodeGraphNode("7");
+
+        graph.addVertex(v0);
+        graph.addVertex(v1);
+        graph.addVertex(v2);
+        graph.addVertex(v3);
+        graph.addVertex(v4);
+        graph.addVertex(v5);
+        graph.addVertex(v6);
+        graph.addVertex(v7);
+        graph.addEdge(v0, v1);
+        graph.addEdge(v1, v2);
+        graph.addEdge(v2, v3);
+        graph.addEdge(v3, v4);
+        graph.addEdge(v4, v5);
+
+        // DETOUR 1
+        graph.addEdge(v1, v3);
+
+        // DETOUR 2
+        graph.addEdge(v2, v6);
+        graph.addEdge(v6, v7);
+        graph.addEdge(v7, v5);
+
+        DepthFirstTraversalLabelTask task = new DepthFirstTraversalLabelTask(v0, graph);
+
+        Map<GraphNodeLike, Set<GraphNodeLike>> dominatorSets = task.allDominators();
+        assertEquals(ImmutableSet.of(v0), dominatorSets.get(v0));
+        assertEquals(ImmutableSet.of(v0, v1), dominatorSets.get(v1));
+        assertEquals(ImmutableSet.of(v0, v1, v2), dominatorSets.get(v2));
+        assertEquals(ImmutableSet.of(v0, v1, v3), dominatorSets.get(v3));
+        assertEquals(ImmutableSet.of(v0, v1, v3, v4), dominatorSets.get(v4));
+        assertEquals(ImmutableSet.of(v0, v1, v5), dominatorSets.get(v5));
+        assertEquals(ImmutableSet.of(v0, v1, v2, v6), dominatorSets.get(v6));
+        assertEquals(ImmutableSet.of(v0, v1, v2, v6, v7), dominatorSets.get(v7));
+    }
+
+    @Test
+    public void canFindImmediateDominators() {
+    /*
+    0 -> 1 -> 2 -> 3 -> 4 -> 5
+         |    |    ↑         ↑
+         |____|____|         |
+              └---> 6 --> 7 -┘
+     */
+        Graph<GraphNodeLike, DefaultEdge> graph = new DefaultDirectedGraph<>(DefaultEdge.class);
+        CodeGraphNode v0 = new CodeGraphNode("0");
+        CodeGraphNode v1 = new CodeGraphNode("1");
+        CodeGraphNode v2 = new CodeGraphNode("2");
+        CodeGraphNode v3 = new CodeGraphNode("3");
+        CodeGraphNode v4 = new CodeGraphNode("4");
+        CodeGraphNode v5 = new CodeGraphNode("5");
+        CodeGraphNode v6 = new CodeGraphNode("6");
+        CodeGraphNode v7 = new CodeGraphNode("7");
+
+        graph.addVertex(v0);
+        graph.addVertex(v1);
+        graph.addVertex(v2);
+        graph.addVertex(v3);
+        graph.addVertex(v4);
+        graph.addVertex(v5);
+        graph.addVertex(v6);
+        graph.addVertex(v7);
+        graph.addEdge(v0, v1);
+        graph.addEdge(v1, v2);
+        graph.addEdge(v2, v3);
+        graph.addEdge(v3, v4);
+        graph.addEdge(v4, v5);
+
+        // DETOUR 1
+        graph.addEdge(v1, v3);
+
+        // DETOUR 2
+        graph.addEdge(v2, v6);
+        graph.addEdge(v6, v7);
+        graph.addEdge(v7, v5);
+
+        DepthFirstTraversalLabelTask task = new DepthFirstTraversalLabelTask(v0, graph);
+
+        List<ImmutablePair<GraphNodeLike, GraphNodeLike>> immediateDominators = task.immediateDominators();
+        assertTrue(immediateDominators.contains(ImmutablePair.of(v0, v0)));
+        assertTrue(immediateDominators.contains(ImmutablePair.of(v1, v0)));
+        assertTrue(immediateDominators.contains(ImmutablePair.of(v2, v1)));
+        assertTrue(immediateDominators.contains(ImmutablePair.of(v3, v1)));
+        assertTrue(immediateDominators.contains(ImmutablePair.of(v4, v3)));
+        assertTrue(immediateDominators.contains(ImmutablePair.of(v5, v1)));
+        assertTrue(immediateDominators.contains(ImmutablePair.of(v6, v2)));
+        assertTrue(immediateDominators.contains(ImmutablePair.of(v7, v6)));
     }
 }
