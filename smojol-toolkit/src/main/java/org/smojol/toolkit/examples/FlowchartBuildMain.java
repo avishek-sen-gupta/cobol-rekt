97ce9ea1285e869f15d465a34c9e5bee35cddd7d
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-10-17 22:27:34 +0530

    Setting correct tasks for FlowchartBuildMain

--- smojol-toolkit/src/main/java/org/smojol/toolkit/examples/FlowchartBuildMain.java
+++ smojol-toolkit/src/main/java/org/smojol/toolkit/examples/FlowchartBuildMain.java
@@ -23,8 +23,8 @@ public class FlowchartBuildMain {
                 "/Users/asgupta/code/smojol/out/report",
                 ImmutableList.of(new File("/Users/asgupta/code/smojol/smojol-test-code")),
                 "/Users/asgupta/code/smojol/che-che4z-lsp-for-cobol-integration/server/dialect-idms/target/dialect-idms.jar",
-                LanguageDialect.IDMS, new FullProgram(FlowchartOutputFormat.MERMAID), new UUIDProvider(), new OccursIgnoringFormat1DataStructureBuilder(), new ProgramSearch(), new LocalFilesystemOperations())
-                .runForPrograms(ImmutableList.of(CommandLineAnalysisTask.WRITE_RAW_AST), ImmutableList.of("test-exp.cbl"));
+                LanguageDialect.IDMS, new FullProgram(FlowchartOutputFormat.PNG), new UUIDProvider(), new OccursIgnoringFormat1DataStructureBuilder(), new ProgramSearch(), new LocalFilesystemOperations())
+                .runForPrograms(ImmutableList.of(CommandLineAnalysisTask.DRAW_FLOWCHART), ImmutableList.of("test-exp.cbl"));
         System.out.println("DONE");
     }
 }
