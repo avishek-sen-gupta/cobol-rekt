ab6ec05343816df3a2ba7f51b4b349af621e1963
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-10-21 01:20:40 +0530

    Fixing Checkstyle errors

--- smojol-core/src/main/java/org/smojol/common/graph/RemoveGotoTask.java
+++ smojol-core/src/main/java/org/smojol/common/graph/RemoveGotoTask.java
@@ -1,12 +1,6 @@
 package org.smojol.common.graph;
 
-import org.jgrapht.Graph;
-import org.jgrapht.graph.DefaultDirectedGraph;
-import org.smojol.common.id.Identifiable;
 import org.smojol.common.transpiler.TranspilerFlowgraph;
-
-import java.util.*;
-import java.util.stream.Collectors;
 
 public class RemoveGotoTask {
 
