bcc7fae33da794e01d7aebb574430669c75562c5
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-10-31 03:49:12 +0530

    Added logger

--- smojol-api/src/main/java/org/smojol/api/ApiMain.java
+++ smojol-api/src/main/java/org/smojol/api/ApiMain.java
@@ -2,9 +2,16 @@ package org.smojol.api;
 
 import io.javalin.Javalin;
 
+import java.util.logging.Logger;
+
 public class ApiMain {
+    private static final java.util.logging.Logger LOGGER = Logger.getLogger(ApiMain.class.getName());
     public static void main(String[] args) {
-        Javalin.create()
+        Javalin.create(config -> {
+                    config.requestLogger.http((ctx, ms) -> {
+                        LOGGER.info("Got a request: " + ctx.path());
+                    });
+                })
                 .get("/api/heartbeat", ctx -> ctx.result("Hello World!"))
                 .start(7070);
     }
