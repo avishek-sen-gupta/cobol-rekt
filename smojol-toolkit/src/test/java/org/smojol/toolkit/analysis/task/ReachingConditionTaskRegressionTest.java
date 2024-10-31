23e87d1f208975e76a62526b2f63db697f7285ae
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-11-01 02:23:03 +0530

    Uncommenting a thing

--- smojol-toolkit/src/test/java/org/smojol/toolkit/analysis/task/ReachingConditionTaskRegressionTest.java
+++ smojol-toolkit/src/test/java/org/smojol/toolkit/analysis/task/ReachingConditionTaskRegressionTest.java
@@ -8,11 +8,13 @@ import org.smojol.common.dialect.LanguageDialect;
 import org.smojol.common.flowchart.FlowchartOutputFormat;
 import org.smojol.common.graph.GraphSlice;
 import org.smojol.common.graph.GraphSliceTask;
+import org.smojol.common.graph.ReachingConditionDefinitionTask;
 import org.smojol.common.id.UUIDProvider;
 import org.smojol.common.resource.LocalFilesystemOperations;
 import org.smojol.common.transpiler.PrintTranspilerNode;
 import org.smojol.common.transpiler.TranspilerFlowgraph;
 import org.smojol.common.transpiler.TranspilerInstruction;
+import org.smojol.common.transpiler.TranspilerNode;
 import org.smojol.toolkit.analysis.pipeline.BaseAnalysisModel;
 import org.smojol.toolkit.analysis.pipeline.ProgramSearch;
 import org.smojol.toolkit.analysis.task.analysis.CodeTaskRunner;
@@ -26,6 +28,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static org.smojol.toolkit.task.CommandLineAnalysisTask.BUILD_BASE_ANALYSIS;
 
@@ -48,7 +51,7 @@ public class ReachingConditionTaskRegressionTest {
         TranspilerInstruction start = instructions.getFirst();
         TranspilerInstruction printInstruction = instructions.stream().filter(instr -> instr.ref() instanceof PrintTranspilerNode).findFirst().get();
         GraphSlice<TranspilerInstruction, DefaultEdge> slice = new GraphSliceTask<>(transpilerFlowgraph.instructionFlowgraph(), DefaultEdge.class).run(start, printInstruction);
-//        Set<TranspilerNode> reachingConditions = new ReachingConditionDefinitionTask<>(slice).run();
+        Map<TranspilerInstruction, TranspilerNode> reachingConditions = new ReachingConditionDefinitionTask<>(slice).run();
         System.out.println("DONE");
     }
 }
