122be442397144a22dcad6d43c2bce193ef4d138
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-11-02 17:58:20 +0530

    Small renames

--- smojol-app/cobol-lekt/src/ts/NodeBuilder.ts
+++ smojol-app/cobol-lekt/src/ts/NodeBuilder.ts
@@ -1,19 +1,17 @@
-import cytoscape from "cytoscape";
-
 type CytoEdge = { data: { id: string, source: string, target: string }; }
 type CytoNode = { data: { id: string, [key: string]: any } }
 
 export type ModelNode = { id: string; [key: string]: any };
 
-export function recalculatedNodes(current: ModelNode): CytoNode[] {
+export function cytoNodes(current: ModelNode): CytoNode[] {
     const currentGraphNodes = [{data: current}];
     if (current.childTranspilerNodes.length === 0) {
         return currentGraphNodes;
     }
-    return currentGraphNodes.concat(current.childTranspilerNodes.flatMap((e: ModelNode) => recalculatedNodes(e)));
+    return currentGraphNodes.concat(current.childTranspilerNodes.flatMap((e: ModelNode) => cytoNodes(e)));
 }
 
-export function recalculatedEdges(current: ModelNode, thread: ModelNode[]): CytoEdge[] {
+export function cytoEdges(current: ModelNode, thread: ModelNode[]): CytoEdge[] {
     const parentNode = thread.length === 0 ? null : thread.at(-1);
     const myEdges = parentNode == null ? [] : [{
         data: {
@@ -25,12 +23,12 @@ export function recalculatedEdges(current: ModelNode, thread: ModelNode[]): Cyto
     if (current.childTranspilerNodes.length == 0) {
         return myEdges;
     }
-    return myEdges.concat(current.childTranspilerNodes.flatMap((e: ModelNode) => recalculatedEdges(e, thread.concat(current))));
+    return myEdges.concat(current.childTranspilerNodes.flatMap((e: ModelNode) => cytoEdges(e, thread.concat(current))));
 }
 
 export function asCytoscapeModel(current: ModelNode): ((CytoNode | CytoEdge)[]) {
     if (current.childTranspilerNodes === null) return [];
-    const cytoNodes = recalculatedNodes(current);
-    const cytoEdges = recalculatedEdges(current, []);
-    return cytoNodes.concat(cytoEdges);
+    const cNodes = cytoNodes(current);
+    const cEdges = cytoEdges(current, []);
+    return cNodes.concat(cEdges);
 }
