9d4d2c5f3f456f19564caff77347771009ceec03
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-11-04 22:09:40 +0530

    Adding back annotations

--- smojol-api/src/main/java/org/smojol/api/ApiMain.java
+++ smojol-api/src/main/java/org/smojol/api/ApiMain.java
@@ -3,6 +3,7 @@ package org.smojol.api;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import io.javalin.json.JsonMapper;
+import org.jetbrains.annotations.NotNull;
 import org.smojol.api.database.DbContext;
 
 import java.io.IOException;
@@ -28,14 +29,16 @@ public class ApiMain {
     private static JsonMapper getJsonMapper(Gson gson) {
         return new JsonMapper() {
 
+            @NotNull
             @Override
-            public String toJsonString(Object obj, Type type) {
+            public String toJsonString(@NotNull Object obj, @NotNull Type type) {
                 return gson.toJson(obj, type);
             }
 
 
+            @NotNull
             @Override
-            public <T> T fromJsonString(String json, Type targetType) {
+            public <T> T fromJsonString(@NotNull String json, @NotNull Type targetType) {
                 return gson.fromJson(json, targetType);
             }
         };
