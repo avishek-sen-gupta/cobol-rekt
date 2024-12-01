5eb306fd9e75992528067aa5fb1c1e229b565b7e
avishek-sen-gupta <asgupta@thoughtworks.com>
2024-12-01 13:52:05 +0530

    Adding reproducible test for issue 69

---
+++ smojol-toolkit/src/main/java/org/smojol/toolkit/examples/ExecCicsBuildMain_Issue69.java
@@ -0,0 +1,39 @@
+package org.smojol.toolkit.examples;
+
+import com.google.common.collect.ImmutableList;
+import org.smojol.common.dialect.LanguageDialect;
+import org.smojol.common.flowchart.FlowchartOutputFormat;
+import org.smojol.common.id.UUIDProvider;
+import org.smojol.common.resource.LocalFilesystemOperations;
+import org.smojol.toolkit.analysis.pipeline.BaseAnalysisModel;
+import org.smojol.toolkit.analysis.pipeline.ProgramSearch;
+import org.smojol.toolkit.analysis.task.analysis.CodeTaskRunner;
+import org.smojol.toolkit.interpreter.FullProgram;
+import org.smojol.toolkit.interpreter.structure.OccursIgnoringFormat1DataStructureBuilder;
+import org.smojol.toolkit.task.AnalysisTaskResult;
+import org.smojol.toolkit.task.AnalysisTaskResultOK;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import static org.smojol.toolkit.task.CommandLineAnalysisTask.BUILD_BASE_ANALYSIS;
+
+public class ExecCicsBuildMain_Issue69 {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Map<String, List<AnalysisTaskResult>> result = new CodeTaskRunner("/Users/asgupta/code/smojol/smojol-test-code",
+                "/Users/asgupta/code/smojol/out/report",
+                ImmutableList.of(new File("/Users/asgupta/code/smojol/smojol-test-code")),
+                "/Users/asgupta/code/smojol/che-che4z-lsp-for-cobol-integration/server/dialect-idms/target/dialect-idms.jar",
+                LanguageDialect.COBOL, new FullProgram(FlowchartOutputFormat.PNG, new UUIDProvider()),
+                new UUIDProvider(),
+                new OccursIgnoringFormat1DataStructureBuilder(),
+                new ProgramSearch(),
+                new LocalFilesystemOperations())
+                .runForPrograms(ImmutableList.of(BUILD_BASE_ANALYSIS), ImmutableList.of("exec-cics-issue-69.cbl"));
+        AnalysisTaskResultOK ok = (AnalysisTaskResultOK) result.values().stream().toList().getFirst().getFirst();
+        BaseAnalysisModel base = ok.getDetail();
+        System.out.println("DONE");
+    }
+}
